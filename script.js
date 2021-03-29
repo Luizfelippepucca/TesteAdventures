77ef51238c4ebd130fcaf56fdc972a486e45f0c9
Luizfelippepucca <lu1z_21@hotmail.com>
2021-03-28 23:11:17 -0300

    ajustando tela 768px

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ fetch("https://radioprotecaonapratica.com.br/wp-json/wp/v2/posts?_embed")
    const areaCard = document.querySelector('ul');
    const arrayCard = data;
    const itemsCard =  arrayCard.map((itemCard)=>{
-    
-  console.log(itemCard);
+     //console.log(itemCard)
         return  `<li>
                     <div class='card-content'>
-                    <h2> ${itemCard.title.rendered}</h2>
-                    <h3> ${itemCard.excerpt.rendered}</h3>
-                    <h4> ${itemCard.date}</h4>
-                    <h5> ${itemCard._embedded.author[0].name}</h5>
-                    <img src='${itemCard._embedded.author[0].avatar_urls = 'https://secure.gravatar.com/avatar/25a70ae2e16e3f9d3bfad271c0f7ed1e?s=96&d=mm&r=g'}' alt='img_author'/>
-                    <button>Ver Post</button>
+                   
+                    <img src='${itemCard._embedded.author[0].avatar_urls = 'https://secure.gravatar.com/avatar/25a70ae2e16e3f9d3bfad271c0f7ed1e?s=96&d=mm&r=g'}' alt='imagem autor'/>
+                    <h5 > ${itemCard._embedded.author[0].name}</h5>
+                    <h2 class='title'>  ${itemCard.title.rendered}</h2>
+                    <h3 class='excerpt'> ${itemCard.excerpt.rendered}</h3>
+                    <a href='${itemCard.link}' target='_blanck' >Ver Post Completo</a>
+                    <h4>  Data :${itemCard.date}</h4>
                     </div>
           
                  </li>`;
    })
    
+   
   areaCard.innerHTML = itemsCard;
-});
 
  
- 
+});
 
 }
 
